6ab49ca78a3295b3ad40c67e03159382a2c7db1b
Sebastian <sebastian_ch_c@hotmail.com>
2023-09-25 14:09:57 -0500

    Se agrego Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+
+      console.log('Formulario válido, datos:', this.loginForm.value);
+    } else {
+
+    }
+  }
+}
+
